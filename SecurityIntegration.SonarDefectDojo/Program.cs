9ffee844bfbd66aa9780fe65ad8bf0f791d4e1ef
Kleber Aquino <klebersjunior@hotmail.com>
2022-03-14 08:44:10 -0300

    update

---
+++ SecurityIntegration.SonarDefectDojo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecurityIntegration.SonarDefectDojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
